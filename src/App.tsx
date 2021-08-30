a74fa669c5605a0666a532a9ffb11b98e5d87dea
Fernando Pérez <fernando.perez@hqb.cl>
2021-08-30 10:12:39 -0400

    tarea 8

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,9 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from 'react-router-dom';
-import Inicio from './components/Inicio';
 import Componente1 from './components/Componente1';
-import Componente2 from './components/Componente2';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact>
-          <Inicio />
-        </Route>
-        <Route path="/componente1">
-          <Componente1 />
-        </Route>
-        <Route path="/componente2">
-          <Componente2 />
-        </Route>
-      </Switch>
-    </Router>
+    <Componente1/>
   );
 }
 
--- src/components/Componente1.tsx
+++ src/components/Componente1.tsx
@@ -1,15 +1,67 @@
 import React from 'react'
-import {
-    Link
-} from 'react-router-dom';
 
 const Componente1 = () => {
+    const [numeroRandom, setNumeroRandom] = React.useState<number>();
+    const [numeroInput, setNumeroInput] = React.useState<any>();
+    const [procesando, setProcesando] = React.useState<boolean>(false);
+
+    React.useEffect(() => {
+        generarNumeroRandom();
+    }, []);
+
+    const generarNumeroRandom = () => {
+        const _numeroRandom = Math.floor(Math.random() * (10 - 1)) + 1;
+        console.log("el número es: ", _numeroRandom);
+        setNumeroRandom(_numeroRandom);
+        
+    };
+
+    const adivinarNumero = () => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (isNaN(numeroInput)){
+                    return reject(new Error('Valor debe ser numérico'));
+                }
+                const _numeroInput: number = Number(numeroInput);
+                return resolve(_numeroInput === numeroRandom);
+            }, 4000)
+        })
+    };
+
+    const onChange = (event: React.ChangeEvent<HTMLInputElement>) =>{
+        setNumeroInput(event.target.value);
+    };
+
+    const onClick = () => {
+        setProcesando(true);
+        adivinarNumero()
+        .then(response => {
+            if (response){
+                alert('¡adivinaste!');
+            }
+            else{
+                alert('no adivinaste :(');
+            }
+            
+        })
+        .catch(error => alert(error))
+        .finally(() => {
+            setProcesando(false);
+            setNumeroInput('');
+        })
+    };
+
+
     return (
         <div>
-            Soy el componente 1
-            <Link to={'/'}>
-                <button>Ir a Inicio</button>
-            </Link>
+            Ingresar un número de 1 a 10
+           <input type="text" onChange={onChange} value={numeroInput} placeholder={'Ingresar número'}/>
+           <button type={'button'} onClick={onClick} disabled={procesando}>Adivinar</button>
+           <br/>
+           {
+               procesando &&
+               <h2>Procesando datos...</h2>
+           }
         </div>
     )
 }
